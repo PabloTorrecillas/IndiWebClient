bce43e997a263acd7b0534c185b4cdf07392d011
PabloTorrecillas <torrecillas90@gmail.com>
2015-10-31 08:53:03 +0100

    Reorganizacion del código y comentarios en setSwitchVector

--- include/parserSetVector.js
+++ include/parserSetVector.js
@@ -48,32 +48,27 @@ function parserSetSwitchVector(node){
 
   var propertyValue = document.getElementById(getPropertyId(deviceName,propertyName) + "_value");
 
+  //Pongo a vacío el propertyValue para ir añadiendo los que están activos.
+  //Hago esto por si son AnyOfMany para poder ir metiendo todos ya que si no lo hago me saca el ultimo solo.
   propertyValue.innerHTML = '';
+
   for(var i = 0; i < nodes.length; i++){
     if(nodes[i].innerHTML.trim().toLowerCase() == 'on'){
       var element = document.getElementById(getElementId(deviceName,propertyName,nodes[i].getAttribute("name")) + "_value");
-      debug("PARSEANDO");
-      debug(element);
-      if(element.nodeName == 'OPTION'){
-        debug("ES UN OPTION");
-        debug(node);
-        
-          propertyValue.innerHTML = element.innerHTML;
-          if(propertyValue.innerHTML == ''){
-            debug("propertyValue");
-            debug(propertyValue);
-          }
-          debug("FIN ES UN OPTION");
 
+      //Si es OPTION (lo he puesto en mayus porque he leido que funciona bien), directamente lo cambio
+      //OPTION son los OneOfMany y AtMostOne
+      if(element.nodeName == 'OPTION'){
+          propertyValue.innerHTML = element.innerHTML;
       }
+      //Si no es un option
       else{
-        debug("NO ES UN OPTION");
-          var labelNode = element.parentNode;
-          var spanNode = labelNode.getElementsByTagName("span")[0];
-          debug("SPAN!!!");
-          debug(spanNode);
-          propertyValue.innerHTML += ' ' + spanNode.innerHTML;
-          debug("FIN NO ES UN OPTION");
+        //Cojo el parentNode para así poder acceder a la etiqueta span.
+        var labelNode = element.parentNode;
+        var spanNode = labelNode.getElementsByTagName("span")[0];
+
+        //Voy añadiendo a property value y por eso pongo el espacio en blanco para que se me vayan mostrando los que estan activos.
+        propertyValue.innerHTML += ' ' + spanNode.innerHTML;
       }
     }
   }
