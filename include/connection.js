030035b4c70732f2c2e440f166797e2f84891078
PabloTorrecillas <torrecillas90@gmail.com>
2015-11-28 15:58:12 +0100

    Añadiendo pequeñas modificaciones en la interfaz

--- include/connection.js
+++ include/connection.js
@@ -1,208 +1,172 @@
 var ws;  
             
-        function disconnect() {
-          if (ws != null) {
-            console.log("Desconectando");
-            ws.close();
-            ws = null;
-          }
-        }
+    function disconnect() {
+      if (ws != null) {
+        console.log("Desconectando");
+        ws.close();
+        ws = null;
+      }
+    }
             
-        function connect() {
-            disconnect();
-           
-            ws = new Websock();
-           
-             ws.on('message', funcionLectura);
-           
-            ws.on('open', function(e) {
-                console.log(">> WebSockets.onopen");
-                ws.send_string("<getProperties version=\"1.7\"/>");
-                console.log("<< WebSockets.onopen");
-            });
-           
-            ws.on('close', function(e) {
-                console.log(">> WebSockets.onclose");
-                console.log("<< WebSockets.onclose");
-            });
-           
-            ws.on('error', function(e) {
-                console.log(">> WebSockets.onerror");
-                console.log(e);
-                console.log("<< WebSockets.onerror");
-            });
-           
-            console.log("Conectando...");
-            ws.open("ws://localhost:4000");
-        }
+    function connect() {
+        disconnect();
+       
+        ws = new Websock();
+       
+         ws.on('message', funcionLectura);
+       
+        ws.on('open', function(e) {
+            console.log(">> WebSockets.onopen");
+            ws.send_string("<getProperties version=\"1.7\"/>");
+            console.log("<< WebSockets.onopen");
+        });
+       
+        ws.on('close', function(e) {
+            console.log(">> WebSockets.onclose");
+            console.log("<< WebSockets.onclose");
+        });
+       
+        ws.on('error', function(e) {
+            console.log(">> WebSockets.onerror");
+            console.log(e);
+            console.log("<< WebSockets.onerror");
+        });
+       
+        console.log("Conectando...");
+        ws.open("ws://localhost:4000");
+    }
             
-        function funcionLectura() {
-            var length = ws.rQlen();
-            var cadena = ws.rQshiftStr(length);
-          
-            //console.log("---" + cadena + "---");
-            
-            
+    function funcionLectura() {
+        var length = ws.rQlen();
+        var cadena = ws.rQshiftStr(length);
+      
+        //console.log("---" + cadena + "---");
+        
+        
 
-            parserXML("<a>" + cadena + "</a>");
-        }
+        parserXML("<a>" + cadena + "</a>");
+    }
 
-        function funcionEscritura(deviceName, propertyName){
-            //debug(deviceName);
-            //debug(propertyName);
+    function funcionEscritura(deviceName, propertyName){
+        var boxId = getPropertyId(deviceName,propertyName) + "_box";
 
-            var boxId = getPropertyId(deviceName,propertyName) + "_box";
+        var box = document.getElementById(boxId);
 
-            var box = document.getElementById(boxId);
+        var children = box.childNodes; 
+        var elementNames = new Array();
+        var values = new Array();
 
-            var children = box.childNodes; 
-            var elementNames = new Array();
-            var values = new Array();
+        for(var i = 0; i < children.length; i++){
+            var hijo = children[i];
+            var elementName = hijo.getAttribute("data_elementname");
 
-            for(var i = 0; i < children.length; i++){
-                var hijo = children[i];
-                var elementName = hijo.getAttribute("data_elementname");
+            elementNames[i] = elementName;
 
-                //debug(elementName);
+            var inputId = getElementId(deviceName,propertyName,elementName) + "_input";
+            var input = document.getElementById(inputId);
 
-                elementNames[i] = elementName;
-
-                var inputId = getElementId(deviceName,propertyName,elementName) + "_input";
-                var input = document.getElementById(inputId);
-
-                values[i] = input.value;
-
-                //debug(values[i]);
-            }
-
-            var cadena = '<newTextVector device="' + deviceName + '" name="' + propertyName + '">';
-
-            for(var i = 0; i < elementNames.length; i++){
-                cadena += '<oneText name="'+ elementNames[i] +'">'+ values[i] +'</oneText>';
-            }
-
-            cadena += '</newTextVector>';
-
-            /*
-            Poner en amarillo la bombilla. Poner un retardo de un segundo
-            */
-
-            ws.send_string(cadena);
-            ws.flush();
+            values[i] = input.value;
         }
 
-        function writeNumber(deviceName, propertyName){
-           // debug(deviceName);
-            //debug(propertyName);
+        var cadena = '<newTextVector device="' + deviceName + '" name="' + propertyName + '">';
 
-            var boxId = getPropertyId(deviceName,propertyName) + "_box";
-
-            var box = document.getElementById(boxId);
-
-            var children = box.childNodes; 
-            var elementNames = new Array();
-            var values = new Array();
-
-            for(var i = 0; i < children.length; i++){
-                var hijo = children[i];
-                var elementName = hijo.getAttribute("data_elementname");
-
-                //debug(elementName);
-
-                elementNames[i] = elementName;
-
-                var inputId = getElementId(deviceName,propertyName,elementName) + "_input";
-                var input = document.getElementById(inputId);
-
-                values[i] = input.value;
-
-                //debug(values[i]);
-            }
-
-            var cadena = '<newNumberVector device="' + deviceName + '" name="' + propertyName + '">';
-
-            for(var i = 0; i < elementNames.length; i++){
-                cadena += '<oneNumber name="'+ elementNames[i] +'">'+ values[i] +'</oneNumber>';
-            }
-
-            cadena += '</newNumberVector>';
-            
-            ws.send_string(cadena);
-            ws.flush();
+        for(var i = 0; i < elementNames.length; i++){
+            cadena += '<oneText name="'+ elementNames[i] +'">'+ values[i] +'</oneText>';
         }
 
-        function writeSwitchSelect(deviceName, propertyName){
-            debug(deviceName);
-            debug(propertyName);
+        cadena += '</newTextVector>';
 
-            var selectId = getPropertyId(deviceName,propertyName) + "_select";
+        ws.send_string(cadena);
+        ws.flush();
+    }
 
-            var select = document.getElementById(selectId);
+    function writeNumber(deviceName, propertyName){
+        var boxId = getPropertyId(deviceName,propertyName) + "_box";
+       
+        var box = document.getElementById(boxId);
+        var children = box.childNodes; 
+        var elementNames = new Array();
+        var values = new Array();
 
-            debug(select.value);
+        for(var i = 0; i < children.length; i++){
+            var hijo = children[i];
+            var elementName = hijo.getAttribute("data_elementname");
 
-            var cadena = '<newSwitchVector device="' + deviceName + '" name="' + propertyName + '">';
+            elementNames[i] = elementName;
 
-            var children = select.childNodes;
+            var inputId = getElementId(deviceName,propertyName,elementName) + "_input";
+            var input = document.getElementById(inputId);
 
-            for(var i = 0; i < children.length; i++){
-                debug(children[i].innerHTML);
-                if(children[i].innerHTML.trim().length > 0){
-                    var value = children[i].getAttribute("value");
-                    var switchValue = 'Off';
-                    if(value == select.value){
-                        switchValue = 'On';
-                    }
+            values[i] = input.value;
+        }
 
-                    cadena += '<oneSwitch name="'+ value +'">'+ switchValue +'</oneSwitch>';
+        var cadena = '<newNumberVector device="' + deviceName + '" name="' + propertyName + '">';
+
+        for(var i = 0; i < elementNames.length; i++){
+            cadena += '<oneNumber name="'+ elementNames[i] +'">'+ values[i] +'</oneNumber>';
+        }
+
+        cadena += '</newNumberVector>';
+        
+        ws.send_string(cadena);
+        ws.flush();
+    }
+
+    function writeSwitchSelect(deviceName, propertyName){
+        var selectId = getPropertyId(deviceName,propertyName) + "_select";
+
+        var select = document.getElementById(selectId);
+        var children = select.childNodes;
+
+        var cadena = '<newSwitchVector device="' + deviceName + '" name="' + propertyName + '">';
+
+        for(var i = 0; i < children.length; i++){
+            debug(children[i].innerHTML);
+            if(children[i].innerHTML.trim().length > 0){
+                var value = children[i].getAttribute("value");
+                var switchValue = 'Off';
+                if(value == select.value){
+                    switchValue = 'On';
                 }
-            	
+                cadena += '<oneSwitch name="'+ value +'">'+ switchValue +'</oneSwitch>';
             }
-
-            cadena += '</newSwitchVector>';
-
-            debug(cadena);
-            
-            ws.send_string(cadena);
-            ws.flush();
         }
 
-        function writeSwitchCheckboxes(deviceName, propertyName){
-            var boxId = getPropertyId(deviceName,propertyName) + "_box";
+        cadena += '</newSwitchVector>';
 
-            var box = document.getElementById(boxId);
+        ws.send_string(cadena);
+        ws.flush();
+    }
 
-            var cadena = '<newSwitchVector device="' + deviceName + '" name="' + propertyName + '">';
+    function writeSwitchCheckboxes(deviceName, propertyName){
+        var boxId = getPropertyId(deviceName,propertyName) + "_box";
 
-            var children = box.childNodes;
+        var box = document.getElementById(boxId);
+        var children = box.childNodes;
 
-            for(var i = 0; i < children.length; i++){
-                var data_elementname = children[i].getAttribute("data_elementname");
-                debug(data_elementname);
-                if(data_elementname != null){
-                    var inputId = getPropertyId(deviceName,propertyName) + "_"+ data_elementname +"_value";
-                debug(inputId);
+        var cadena = '<newSwitchVector device="' + deviceName + '" name="' + propertyName + '">';
+
+        for(var i = 0; i < children.length; i++){
+            var data_elementname = children[i].getAttribute("data_elementname");
+
+            if(data_elementname != null){
+                var inputId = getPropertyId(deviceName,propertyName) + "_"+ data_elementname +"_value";
+            
                 var input = document.getElementById(inputId);
-                    //var value = children[i].getAttribute("value");
+                var switchValue = 'Off';
 
-                    var switchValue = 'Off';
-                    if(input.checked){
-                        switchValue = 'On';
-                    }
-
-                    cadena += '<oneSwitch name="'+ data_elementname +'">'+ switchValue +'</oneSwitch>';
-                
+                if(input.checked){
+                    switchValue = 'On';
                 }
-                
-                
+
+                cadena += '<oneSwitch name="'+ data_elementname +'">'+ switchValue +'</oneSwitch>';
             }
-
-            cadena += '</newSwitchVector>';
-
-            debug(cadena);
-            
-            ws.send_string(cadena);
-            ws.flush();
         }
+
+        cadena += '</newSwitchVector>';
+        
+        ws.send_string(cadena);
+        ws.flush();
+    }
 
         
